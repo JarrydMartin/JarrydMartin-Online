6a35a362a9491cd10d92226d9df289f2b5a9b032
Jarryd Martin <jarryd@mhacsp.onmicrosoft.com>
2020-04-09 09:14:04 +1200

    added base main compents

--- src/app/jarrydmartin-online/jarrydmartin-online-routing.module.ts
+++ src/app/jarrydmartin-online/jarrydmartin-online-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JarrydmartinOnlineComponent } from './jarrydmartin-online.component';
+import { MaincontentComponent } from './components/maincontent/maincontent.component';
 
 const routes: Routes = [
   {
     path: '',
     component: JarrydmartinOnlineComponent,
+    children: [
+      {
+        path: '',
+        component: MaincontentComponent,
+      },
+    ],
     pathMatch: 'full',
   },
   {
--- src/app/jarrydmartin-online/jarrydmartin-online.module.ts
+++ src/app/jarrydmartin-online/jarrydmartin-online.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { JarrydmartinOnlineRoutingModule } from './jarrydmartin-online-routing.module';
 import { JarrydmartinOnlineComponent } from './jarrydmartin-online.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MaincontentComponent } from './components/maincontent/maincontent.component';
 
 
 @NgModule({
-  declarations: [JarrydmartinOnlineComponent],
+  declarations: [JarrydmartinOnlineComponent, SidenavComponent, ToolbarComponent, MaincontentComponent],
   imports: [
     CommonModule,
     JarrydmartinOnlineRoutingModule
