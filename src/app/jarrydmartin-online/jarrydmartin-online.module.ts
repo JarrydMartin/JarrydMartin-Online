be38dcd9ffe2471403a906cb4e624179efdc385e
Jarryd Martin <jarryd@mhacsp.onmicrosoft.com>
2020-04-09 08:27:02 +1200

    Added Jarry Martin Online module

---
+++ src/app/jarrydmartin-online/jarrydmartin-online.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { JarrydmartinOnlineRoutingModule } from './jarrydmartin-online-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    JarrydmartinOnlineRoutingModule
+  ]
+})
+export class JarrydmartinOnlineModule { }
